89645c8c5bdee87f0d72731093b454f3c3ea8125
Emmanuel Micaiah Afriyie <eafriyie@hawk.iit.edu>
2024-11-26 05:54:52 -0600

    Testing 123

--- app/main.js
+++ app/main.js
@@ -4,22 +4,12 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
-    connection.on('data', (data) => {
-        const command = data.toString().trim();
-
-        if (command === "PING") {
-            // Respond to PING command with one PONG as per RESP protocol
-            connection.write('+PONG\r\n');
-            
-            // Send another PONG separately (not as a response to PING)
-            setTimeout(() => {
-                connection.write('+PONG\r\n');
-            }, 100); // Send second PONG after a short delay
-        } else {
-            // Handle unknown commands gracefully
-            connection.write('-ERROR\r\n');
-        }
-    });
+    if (command === "PING"){
+        connection.write('+PONG\r\n');
+    }
+    else{
+        connection.write('ERROR\r\n');
+    }
  });
 //
  server.listen(6379, "127.0.0.1");
