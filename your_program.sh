a7995119c72b4be727a7a69dc41394275b5c57e9
Emmanuel Micaiah Afriyie <eafriyie@hawk.iit.edu>
2024-11-26 06:22:42 -0600

    Testing 123

--- your_program.sh
+++ your_program.sh
@@ -6,10 +6,10 @@
 #
 # Learn more: https://codecrafters.io/program-interface
 
-set -e # Exit early if any commands fail
-echo "success"
+#set -e # Exit early if any commands fail
+#echo "success"
 # Copied from .codecrafters/run.sh
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-exec node app/main.js "$@"
+#exec node app/main.js "$@"
