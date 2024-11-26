0534d0117848a60a5225f792af03d511fab98b70
Emmanuel Micaiah Afriyie <eafriyie@hawk.iit.edu>
2024-11-26 05:59:29 -0600

    Testing 123

--- app/main.js
+++ app/main.js
@@ -4,11 +4,14 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
+    connection.compose('data', (data) => {
+        const command = data.toString().trim();
     if (command === "PING"){
         connection.write('+PONG\r\n');
     } else{
         connection.write('ERROR\r\n');
     }
+  })
  });
 //
  server.listen(6379, "127.0.0.1");
